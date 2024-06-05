7fc1bac1af7e3788da02c6de9332634f2c286e8e
vanessa <108832601+amanda29102007@users.noreply.github.com>
2024-06-05 15:12:12 -0300

    0506

--- main.js
+++ main.js
@@ -34,34 +34,37 @@ function geraSenha(){
 senha = senha + letrasMaiusculas[numeroAleatorio];   
 }   
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-classificaSenha(alfabeto.length);
-
-function classificaSenha(tamanhoAlfabeto){ 
+if (checkbox[1].checked){
+    alfabeto = alfabeto + letrasMinusculas;
+    }
+    if (checkbox[2].checked){
+    alfabeto = alfabeto + numeros;
+    }
+    if (checkbox[3].checked){
+    alfabeto = alfabeto + simbolos;
+    }
+    let senha = '';
+    for (let i = 0; i < tamanhoSenha;i++){
+    let numeroAleatorio = Math.random()*alfabeto.length;
+    numeroAleatorio = Math.floor(numeroAleatorio);
+    senha = senha + alfabeto[numeroAleatorio];
+    }
+    campoSenha.value = senha;
+    campoSenha.value = senha;
+    classificaSenha();
+    classificaSenha(alfabeto.length);
+    }
+function classificaSenha(){
+function classificaSenha(tamanhoAlfabeto){
     let entropia = tamanhoSenha * Math.log2(alfabeto.length);
     forcaSenha.classList.remove('fraca','media','forte');
-    if (entropia > 58){
+    if (tamanhoSenha > 11){
+    if (entropia > 57){
         forcaSenha.classList.add('forte');
     } else if (entropia > 35 && entropia < 57){
         forcaSenha.classList.add('media');
     } else if (entropia < 35){
         forcaSenha.classList.add('fraca');
     }
-    }
-    valorEntropia.textContent = 2**Math.floor(entropia)/(100e6*60*60*24);
+}
+valorEntropia.textContent = 2**Math.floor(entropia)/(100e6*60*60*24);
